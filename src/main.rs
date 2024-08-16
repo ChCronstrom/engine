fa017f9f4391e1980f8e02d401af18483f6e1a5a
Christoffer Cronström de Laval <christoffer.cronstrom@avaloninnovation.com>
2024-08-16 14:43:20 +0200

    Shell for UCI interface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::io;
+
+mod uci;
+
 fn main()
 {
     println!("Hello, world!");
+    let stdin = io::stdin().lock();
+    let stdout = io::stdout().lock();
+    let mut uci = uci::UciClient::new(stdin, stdout);
+    uci.main_loop().expect("IO error");
 }
---
+++ src/uci.rs
@@ -0,0 +1,45 @@
+use std::io;
+use std::io::{BufRead, Write};
+
+pub struct UciClient<'a>
+{
+    stdin: io::StdinLock<'a>,
+    stdout: io::StdoutLock<'a>,
+}
+
+impl<'a> UciClient<'a>
+{
+    pub fn new(stdin: io::StdinLock<'a>, stdout: io::StdoutLock<'a>) -> UciClient<'a>
+    {
+        UciClient { stdin, stdout }
+    }
+
+    pub fn main_loop(&mut self) -> io::Result<()>
+    {
+        let mut input = String::new();
+        loop
+        {
+            input.clear();
+            self.stdin.read_line(&mut input)?;
+            if input.len() == 0 {
+                // EOF on input, exit
+                return Ok(());
+            }
+
+            let mut command_words = input.split_ascii_whitespace();
+            let command =  command_words.next();
+            if let Some(command) = command
+            {
+                match command
+                {
+                    "quit" => {
+                        return Ok(());
+                    }
+                    _ => {
+                        writeln!(self.stdout, "Unknown command: {command}")?;
+                    }
+                }
+            }
+        }
+    }
+}
